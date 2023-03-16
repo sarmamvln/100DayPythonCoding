2e8467b0e2fc6df927851ce23fc9fc0306008021
S <sarmamvln@gmail.com>
2023-03-15 22:25:26 -0500

    mainv1.1-day02
    v1.1

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import  codeChallenge.day01_BandNameSuggetion as d1;
+import codeChallenge.day01_BandNameSuggetion as d1;
+import  codeChallenge.day02_tipCalculator as d2;
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,7 +14,13 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi;
-    d1.bandName.suggetion();
+    print("========================================")
+    #d1.bandName.suggetion();
+    print("========================================")
+    #d2.tipCalculator.totalbill();
+
+
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
