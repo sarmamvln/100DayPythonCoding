0a5bd33e29f8953b9e8b93ad892b3c0d3aba3616
S <sarmamvln@gmail.com>
2023-03-17 23:33:17 -0500

    Day 04 - update
    v1.4

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import codeChallenge.day01_BandNameSuggetion as d1;
-import  codeChallenge.day02_tipCalculator as d2;
+import codeChallenge.day02_tipCalculator as d2;
+import codeChallenge.day03_treasureIslandGame as d3;
+import codeChallenge.day04_RockPaperScissorGamewithComputer as d4;
 
 
 def print_hi(name):
@@ -18,9 +20,10 @@ if __name__ == '__main__':
     #d1.bandName.suggetion();
     print("========================================")
     #d2.tipCalculator.totalbill();
-
-
-
-
+    print("========================================")
+    #d3.TreasureIsland.gameFlow();
+    print("========================================")
+    d4.RockPaperScissorGame.game();
+    print("========================================")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
