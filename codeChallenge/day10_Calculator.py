9583a0177fdfd8a79623afd516a705320a906dfc
sarmamvln <sarmamvln@gmail.com>
2024-04-14 13:08:19 -0400

    day10 calclulator program

--- codeChallenge/day10_Calculator.py
+++ codeChallenge/day10_Calculator.py
@@ -1,6 +1,6 @@
 #Day10-Calculator
 from replit import clear;
-
+from math import sqrt, isqrt, sin, cos, tan;
 
 class Calc:
 
@@ -32,50 +32,101 @@ class Calc:
                    ''';
          print("\t\t\t\t\t\tCalculator\n=========================================================\t\t"+logo);
 
-    def humpropmt():
+    def calci():
         c.pre_img();
+        flag = True;
+        lastval=[];
+        lastval.append(input("Enter the number: "));
+        while (flag):
+            option = input(
+                f"Select the operation corresponding number you want to perform:\n1.Addition (+)\n2.Subtraction (-)\n3.Multiplication (x)\n4.Division (/)\n5.Root\n6.Trignometry\n-------------------\n:      ");
+            if(lastval[-1]=='exit'):
+                flag=False;
+                lastval.remove('exit')
+            elif(option=='exit'):
+                flag=False;
+            else:
+                if(option=='1'):
+                    lastval.append(input("Enter the second number  (type 'exit' to quit): "));
+                    lastval.append(c.addt(lastval))
+                    print(f"The addition of  {lastval[-3]} and {lastval[-2]} is: ", lastval[-1]);
 
-        option=int(input("Select the operation number you want to perform:\n1.Addition (+)\n2.Subtraction (-)\n3.Multiplication (x)\n4.Division (/)\n5.Root\n6.Trignometry\n-------------------\n:      ") );
-        #while(option.is_integer()):
+                if(option =='2'):
+                    lastval.append(input("Enter the second number  (type 'exit' to quit): "));
+                    lastval.append(c.subt(lastval));
+                    print(f"The final value after subtraction of {lastval[-3]} and {lastval[-2]} is: ", lastval[-1]);
 
-        if(option==1):
-            flag=True;
-            values=[];
-            while(flag):
-                values.append(input("Enter the number for addition (type 'exit' to quit): "));
-                if(values[-1]=='exit'):
-                    flag=False;
-                    values.remove('exit')
-                print(f"The sum of values {values} is: ", c.addt(values))
+                if(option =='3'):
+                    lastval.append(input("Enter the second number  (type 'exit' to quit): "));
+                    lastval.append(c.mult(lastval))
+                    print(f"The final value after multiplication of {lastval[-3]} and {lastval[-2]} is: ", lastval[-1])
+
+                if(option =='4'):
+                    lastval.append(input("Enter the second number  (type 'exit' to quit): "));
+                    lastval.append(c.divt(lastval))
+                    print(f"The final value after divison of {lastval[-3]} and {lastval[-2]} is: ", lastval[-1])
+
+                if(option =='5'):
+                    lastval.append(c.rut(lastval))
+                    print(f"The root of values {lastval[-2]} is: ", lastval[-1]);
+
+                if(option =='6'):
+                    option = input(
+                        f"Select the operation corresponding number you want to perform:\n1.Sin\n2.Cos\n3.Tan\n-------------------\n:      ");
+                    if (option == '1'):
+                        lastval.append(c.sinf(lastval))
+                        print(f"The Sin of values {lastval[-2]} is: ", lastval[-1]);
+                    if (option == '2'):
+                        lastval.append(c.cosf(lastval))
+                        print(f"The Cos of values {lastval[-2]} is: ", lastval[-1]);
+                    if (option == '3'):
+                        lastval.append(c.tanf(lastval))
+                        print(f"The Tan of values {lastval[-2]} is: ", lastval[-1]);
 
 
-            print(f"The Final sum of values {values} is: ", c.addt(values))
 
-        if(option ==2):
-            a = int(input("Enter 1st number for subtraction: "));
-            b = int(input("Enter 2nd number for subtraction: "));
-            print(f"The value of {a}-{b} is: ", c.subt(a, b));
 
     def addt(a):
+        if(len(a)<=2):
+            return int(a[0])+int(a[1]);
+        else:
+           return int(a[-1])+int(a[-2]);
 
-        sum=0;
-        for i in range(0, len(a)):
-           sum+=int(a[i]);
-        return (sum);
+    def subt(a):
+        if(len(a)<=2):
+            return int(a[0])-int(a[1]);
+        else:
+           return int(a[-2])-int(a[-1]);
 
-    def subt(a, b):
-        diff = 0;
-        for i in range(0, len(a)):
-            diff -= int(a[i]);
-        return (diff);
+    def mult(a):
+        if(int(a[-1])==0):
+            return 0;
+        elif(len(a)<=2):
+            return int(a[0])*int(a[1]);
+        else:
+           return int(a[-2])*int(a[-1]);
+    def divt(a):
+        if(int(a[-1])==0):
+            return 0;
+        elif(len(a)<=2):
+            return int(a[0])/int(a[1]);
+        else:
+           return int(a[-2])/int(a[-1]);
+    def rut(rootnum):
+        if(rootnum[-1]!='exit'):
+            return sqrt(int(rootnum[-1]));
+        elif(rootnum[-1]<0):
+            return 'i';
+        else:
+            return False;
+    def sinf(a):
+        return sin(int(a[-1]));
+    def cosf(a):
+        return cos(int(a[-1]));
+    def tanf(a):
+        return tan(int(a[-1]));
 
-
-    def mult(a, b):
-        return (a*b);
-
-    def divt(a, b):
-        return (a/b);
 
 c=Calc;
-c.humpropmt();
-#c.calbegin();
+#c.calci();
+
--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import codeChallenge.day01_BandNameSuggetion as d1;
-import codeChallenge.day02_tipCalculator as d2;
-import codeChallenge.day03_treasureIslandGame as d3;
-import codeChallenge.day04_RockPaperScissorGamewithComputer as d4;
-import codeChallenge.day05_PasswordGenerator as d5;
-import codeChallenge.day07_HangMan_Game as d7;
-import codeChallenge.day08_CeagarCipher as d8;
-import codeChallenge.day09_SilentAuction as d9;
-
+#import codeChallenge.day01_BandNameSuggetion as d1;
+#import codeChallenge.day02_tipCalculator as d2;
+#import codeChallenge.day03_treasureIslandGame as d3;
+#import codeChallenge.day04_RockPaperScissorGamewithComputer as d4;
+#import codeChallenge.day05_PasswordGenerator as d5;
+#import codeChallenge.day07_HangMan_Game as d7;
+#import codeChallenge.day08_CeagarCipher as d8;
+#import codeChallenge.day09_SilentAuction as d9;
+import codeChallenge.day10_Calculator as d10;
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -36,6 +36,8 @@ if __name__ == '__main__':
     #d8.ceagerCipher.userMessage();
     #print("========================================")
     #d9.silentAuction.aution();
+    # print("========================================")
+    #d10.Calc.calci();
     # print("========================================")
 
 
