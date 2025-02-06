cacdf4ffc7e8f9c2c06360f573359670876397b5
S <sarmamvln@gmail.com>
2025-02-05 22:02:24 -0500

    day16coffeemachineOOP

--- codeChallenge/day16CoffeeMachineOOP/main.py
+++ codeChallenge/day16CoffeeMachineOOP/main.py
@@ -1,3 +1,27 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from prettytable import  PrettyTable
+is_on=True
+
+menu= Menu()
+brewCoffe= CoffeeMaker()
+ledger= MoneyMachine()
+
+outlineTable= PrettyTable()
+
+brewCoffe.report()
+
+while is_on:
+    options= menu.get_items()
+    choice= input(f"What would you like? ({options}) : ")
+    if(choice=="off"):
+        is_on=False
+    elif choice=="report":
+        brewCoffe.report()
+        ledger.report()
+    else:
+        drink= menu.find_drink(choice)
+        if brewCoffe.is_resource_sufficient(drink) and ledger.make_payment(drink.cost):
+            brewCoffe.make_coffee(drink)
+
